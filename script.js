8c1db4d1885ac9a9a519a61d422cc58f7c31264f
cool-oladick <razborka.alekzs16@gmail.com>
2025-05-04 14:58:09 +0200

    themess

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
+// function changeCSS() {
+//     let linkElement = document.getElementById("path");
+//     let path = linkElement.getAttribute("href");
+//     console.log(path);
+//     if (path == "style-dark.css") {
+//         linkElement.setAttribute("href", "style-light.css");
+//     }
+//     else {
+//         linkElement.setAttribute("href", "style-dark.css");
+//     }
+
+    // var img1 = document.getElementById('img1');
+    // var isrc = img1.getAttribute('src');
+    // if ("style-light.css"  == path) {
+    //     img1.setAttribute('src', 'https://img.icons8.com/?size=100&id=JARydttrNTU2&format=png&color=000000');
+    // }
+    // else {
+    //     img1.setAttribute('src', 'https://img.icons8.com/?size=100&id=60002&format=png&color=000000');
+
+    // }
+// }
+// function changeCSS() {
+//     document.body.classList.toggle("dark-theme");
+// }
+
 function changeCSS() {
     let linkElement = document.getElementById("path");
     let path = linkElement.getAttribute("href");
-    console.log(path);
+
     if (path == "style-dark.css") {
         linkElement.setAttribute("href", "style-light.css");
-    }
-    else {
+    } else {
         linkElement.setAttribute("href", "style-dark.css");
     }
 
-    var img1 = document.getElementById('img1');
-    var isrc = img1.getAttribute('src');
-    if ("style-light.css"  == path) {
-        img1.setAttribute('src', 'https://img.icons8.com/?size=100&id=JARydttrNTU2&format=png&color=000000');
-    }
-    else {
-        img1.setAttribute('src', 'https://img.icons8.com/?size=100&id=60002&format=png&color=000000');
-
+    let img1 = document.getElementById('img1');
+    if (path == "style-dark.css") {
+        img1.setAttribute('src', 'https://img.icons8.com/?size=100&id=CYWbKSvhMDAc&format=png&color=000000'); // біла іконка
+    } else {
+        img1.setAttribute('src', 'https://img.icons8.com/?size=100&id=Wdnu-edbShJS&format=png&color=000000'); // чорна іконка
     }
 }
 
@@ -24,8 +45,8 @@ window.addEventListener('scroll', function () {
     const nav = document.querySelector('nav');
     const links = nav.querySelectorAll('a');
     if (window.scrollY > 150) {
-      nav.classList.add('scrolled');
+        nav.classList.add('scrolled');
     } else {
-      nav.classList.remove('scrolled');
+        nav.classList.remove('scrolled');
     }
-  });
+});
